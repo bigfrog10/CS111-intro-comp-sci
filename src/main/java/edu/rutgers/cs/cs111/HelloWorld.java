40d702180db05d13bb851b94a41966fff6542d89
jojokes <ac2180@scarletmail.rutgers.edu>
2021-09-05 21:24:52 -0400

    command line

--- src/main/java/edu/rutgers/cs/cs111/HelloWorld.java
+++ src/main/java/edu/rutgers/cs/cs111/HelloWorld.java
@@ -3,5 +3,6 @@ package edu.rutgers.cs.cs111;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Hello, world 2!");
     }
 }
