2b5e5a6973858cb5c5e5230340c0e9cb27dfa661
jojokes <ac2180@scarletmail.rutgers.edu>
2021-12-03 22:41:40 -0500

    init

--- src/main/java/edu/rutgers/cs/cs111/HelloWorld2.java
+++ src/main/java/edu/rutgers/cs/cs111/HelloWorld2.java
@@ -1,6 +1,6 @@
 package edu.rutgers.cs.cs111;
 
-public class HelloWorld {
+public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Hello, world 2!");
